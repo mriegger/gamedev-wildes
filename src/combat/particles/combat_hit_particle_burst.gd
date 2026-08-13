ce32e4eaf948dfc40b8f248d8768ab769ca07618
Michael Riegger <mriegger@meta.com>
2026-08-12 17:21:34 -0700

    fix: emit combat particles outside hit geometry

--- src/combat/particles/combat_hit_particle_burst.gd
+++ src/combat/particles/combat_hit_particle_burst.gd
@@ -2,6 +2,7 @@ extends Node3D
 class_name CombatHitParticleBurst
 
 const UPWARD_BIAS: float = 0.65
+const SURFACE_OFFSET: float = 0.1
 
 @onready var _primary: CPUParticles3D = $Primary as CPUParticles3D
 @onready var _accent: CPUParticles3D = $Accent as CPUParticles3D
@@ -9,8 +10,9 @@ const UPWARD_BIAS: float = 0.65
 func play(position: Vector3, hit_direction: Vector3, primary_color: Color, accent_color: Color):
 	assert(position.is_finite())
 	assert(hit_direction.is_finite() and not hit_direction.is_zero_approx())
-	global_position = position
-	var launch_direction := (hit_direction.normalized() + Vector3.UP * UPWARD_BIAS).normalized()
+	var outward_direction := -hit_direction.normalized()
+	global_position = position + outward_direction * SURFACE_OFFSET
+	var launch_direction := (outward_direction + Vector3.UP * UPWARD_BIAS).normalized()
 	_play_emitter(_primary, launch_direction, primary_color)
 	_play_emitter(_accent, launch_direction, accent_color)
 
--- src/combat/particles/tests/combat_hit_particles_integration.gd
+++ src/combat/particles/tests/combat_hit_particles_integration.gd
@@ -53,13 +53,14 @@ func _run():
 	_emit_outcome(combat, player_zombie)
 	var guts := particles._bursts[0]
 	var guts_profile := particle_catalog.get_profile(&"player", &"zombie")
-	_expect(guts.global_position.is_equal_approx(player_zombie.world_position), "guts burst position changed")
+	var expected_position := player_zombie.world_position - Vector3.RIGHT * CombatHitParticleBurst.SURFACE_OFFSET
+	_expect(guts.global_position.is_equal_approx(expected_position), "guts burst position changed")
 	_expect(guts_profile.primary_color.is_equal_approx(Color(0.19, 0.32, 0.055, 1.0)), "guts palette became too bright")
 	_expect(guts_profile.accent_color.is_equal_approx(Color(0.22, 0.065, 0.035, 1.0)), "guts accent palette changed")
 	_expect((guts.get_node("Primary") as CPUParticles3D).emitting and (guts.get_node("Accent") as CPUParticles3D).emitting, "guts burst did not emit")
 	_expect((guts.get_node("Primary") as CPUParticles3D).color.is_equal_approx(guts_profile.primary_color), "guts primary color changed")
 	_expect((guts.get_node("Accent") as CPUParticles3D).color.is_equal_approx(guts_profile.accent_color), "guts accent color changed")
-	var expected_direction := (Vector3.RIGHT + Vector3.UP * CombatHitParticleBurst.UPWARD_BIAS).normalized()
+	var expected_direction := (Vector3.LEFT + Vector3.UP * CombatHitParticleBurst.UPWARD_BIAS).normalized()
 	_expect((guts.get_node("Primary") as CPUParticles3D).direction.is_equal_approx(expected_direction), "guts direction changed")
 	var zombie_player := MeleeContact.new(1, &"zombie", 0, &"player", &"zombie_melee", Vector3(2.0, 3.0, 4.0), Vector3.LEFT)
 	_emit_outcome(combat, zombie_player)
